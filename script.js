e991cb846af365878b7ebd70c8e98b1b163d4583
SoshuKo <chitian.kokkota@gmail.com>
2024-12-10 07:06:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function playTurn(childChoice) {
     if (childChoice === 'Kiún' && parentChoice !== 'Kiún') {
         resultMessage = '親のKiúnに対し、子がKiúnを出さなかったため親の負け！';
     } else if (parentChoice === 'Kiún' && childChoice !== 'Kiún') {
-        resultMessage = '子のKiúnに対し、親がKiúnを出さなかったため親の負け！';
+        resultMessage = '親のKiúnに対し、子がKiúnを出さなかったため親の負け！';
     } else if (parentChoice === childChoice && childChoice === 'Kiún') {
         resultMessage = '親と子が同じ役であるKiúnを出したため勝負は決まりません！ゲームは続行されます。';
         // ゲーム続行の場合、ターン交代せず次のターンへ
